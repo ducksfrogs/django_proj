afb0dae5f41b8a38b35cefae6ea9d71ddb248751
ma <ma_yamaki@yahoo.com>
2021-02-03 12:18:06 +0900

    first commit

---
+++ polls/views.py
@@ -0,0 +1,15 @@
+#from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello World from django")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at qeuesiton %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id )
